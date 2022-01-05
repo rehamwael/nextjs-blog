0bafdba8c19fdd451d8c70d9e1dfe878f3624e1f
rehamhabbas <rehamhabbas@hotmail.com>
2022-01-05 16:21:49 +0200

    work on seo

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -8,8 +8,6 @@ const IndexView = () => {
     <>
       <Head>
         <title>Reham Habbas</title>
-        <meta name="description" content="Reham Habbas - Front-End Engineer" />
-        <meta name="keywords" content="Reham Habbas, Front-End Engineer, UI Developer" />
       </Head>
       <Header />
       <About />
