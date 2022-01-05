49c0d23f3ad72927916dbfa4e858c5a538527e31
rehamhabbas <rehamhabbas@hotmail.com>
2022-01-05 16:16:17 +0200

    work on seo

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,16 @@
 import Header from "../components/Header";
 import About from "../components/About";
 import Contact from "../components/Contact";
-
+import Head from 'next/head'
 
 const IndexView = () => {
   return (
     <>
+      <Head>
+        <title>Reham Habbas</title>
+        <meta name="description" content="Reham Habbas - Front-End Engineer" />
+        <meta name="keywords" content="Reham Habbas, Front-End Engineer, UI Developer" />
+      </Head>
       <Header />
       <About />
       <Contact />
